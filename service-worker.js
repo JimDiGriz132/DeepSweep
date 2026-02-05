ae175e035640e90f6547ec481ebcd48d5fdf4bc3
JimDiGriz132 <deva132@gmail.com>
2026-02-05 16:18:07 +0100

    v0.9.7a

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'deepsweep-cache-v0.9.7a';  // ← promijeni verziju kad mijenjaš fajlove!
+const CACHE_NAME = 'deepsweep-cache-v0.9.7aa';  // ← promijeni verziju kad mijenjaš fajlove!
 
 const urlsToCache = [
   '/', 
